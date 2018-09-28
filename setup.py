9a219ed33880b36827ba25a4ff627c08a396a119
finnqiao <yu.qiao@tufts.edu>
2018-09-28 12:02:36 -0500

    structure imported from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A first look at what the dataset of metro bikeshare in LA can tell us about usage patterns, pricing strateegies',
+    author='finn & shree',
+    license='MIT',
+)
